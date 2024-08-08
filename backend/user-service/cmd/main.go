72741e10ed2c6582936e6b295acab36841343ed4
bblueberries <tata10433@gmail.com>
2024-08-08 20:26:53 +0700

    feat: add db connect+gorm

--- backend/user-service/cmd/main.go
+++ backend/user-service/cmd/main.go
@@ -3,16 +3,45 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+type UserTest struct {
+	ID    string
+	Name  string
+	Point int
+}
+
+func testDBconnected(db *gorm.DB){
+	var user UserTest
+	// Retrieve the first row from the user_test table
+	if err := db.Table("user_test").First(&user).Error; err != nil {
+		log.Fatalf("failed to get data: %v", err)
+	}
+	fmt.Printf("ID: %s, Name: %s, Point: %d\n", user.ID, user.Name, user.Point)
+}
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, World!")
-    })
+	err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	
+	dsn := os.Getenv("DATABASE_CONNECTION_STRING")
+	if dsn == "" {
+		log.Fatal("DATABASE_CONNECTION_STRING environment variable is not set")
+	}
 
-    log.Println("Starting server on :8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatalf("Could not start server: %s\n", err)
-    }
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+	fmt.Println("Database connection successful!")
+
+	//test get 1st row from table 'user_test'
+	testDBconnected(db)
+   
 }
---
+++ backend/user-service/go.mod
@@ -0,0 +1,18 @@
+module github.com/bblueberries/daily-login/backend/user-service
+
+go 1.22.2
+
+require (
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	gorm.io/driver/mysql v1.5.7
+	gorm.io/gorm v1.25.11
+)
+
+require github.com/joho/godotenv v1.5.1
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
